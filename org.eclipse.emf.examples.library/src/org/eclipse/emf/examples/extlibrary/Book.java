36ff79dd43312653f967860a78fdeb9886632c55
gehrk <gehrk@192.168.178.43>
2024-07-19 14:17:12 +0200

    removed Annas changes

--- org.eclipse.emf.examples.library/src/org/eclipse/emf/examples/extlibrary/Book.java
+++ org.eclipse.emf.examples.library/src/org/eclipse/emf/examples/extlibrary/Book.java
@@ -4,21 +4,19 @@
  *
  * $Id$
  */
+
 package org.eclipse.emf.examples.extlibrary;
 
-
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Book</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Book</b></em>'. <!-- end-user-doc -->
  *
  * <p>
  * The following features are supported:
  * <ul>
- *   <li>{@link org.eclipse.emf.examples.extlibrary.Book#getTitle <em>Title</em>}</li>
- *   <li>{@link org.eclipse.emf.examples.extlibrary.Book#getPages <em>Pages</em>}</li>
- *   <li>{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}</li>
- *   <li>{@link org.eclipse.emf.examples.extlibrary.Book#getAuthor <em>Author</em>}</li>
+ * <li>{@link org.eclipse.emf.examples.extlibrary.Book#getTitle <em>Title</em>}</li>
+ * <li>{@link org.eclipse.emf.examples.extlibrary.Book#getPages <em>Pages</em>}</li>
+ * <li>{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}</li>
+ * <li>{@link org.eclipse.emf.examples.extlibrary.Book#getAuthor <em>Author</em>}</li>
  * </ul>
  * </p>
  *
@@ -27,14 +25,14 @@ package org.eclipse.emf.examples.extlibrary;
  * @generated
  */
 public interface Book extends CirculatingItem {
+	
 	/**
-	 * Returns the value of the '<em><b>Title</b></em>' attribute.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Title</b></em>' attribute. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Title</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Title</em>' attribute isn't clear, there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Title</em>' attribute.
 	 * @see #setTitle(String)
 	 * @see org.eclipse.emf.examples.extlibrary.EXTLibraryPackage#getBook_Title()
@@ -42,26 +40,24 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	String getTitle();
-
+	
 	/**
-	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getTitle <em>Title</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Title</em>' attribute.
+	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getTitle <em>Title</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Title</em>' attribute.
 	 * @see #getTitle()
 	 * @generated
 	 */
 	void setTitle(String value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Pages</b></em>' attribute.
-	 * The default value is <code>"100"</code>.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Pages</b></em>' attribute. The default value is <code>"100"</code>. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Pages</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Pages</em>' attribute isn't clear, there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Pages</em>' attribute.
 	 * @see #setPages(int)
 	 * @see org.eclipse.emf.examples.extlibrary.EXTLibraryPackage#getBook_Pages()
@@ -69,26 +65,24 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	int getPages();
-
+	
 	/**
-	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getPages <em>Pages</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Pages</em>' attribute.
+	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getPages <em>Pages</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Pages</em>' attribute.
 	 * @see #getPages()
 	 * @generated
 	 */
 	void setPages(int value);
-
+	
 	/**
-	 * Returns the value of the '<em><b>Category</b></em>' attribute.
-	 * The literals are from the enumeration {@link org.eclipse.emf.examples.extlibrary.BookCategory}.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Category</b></em>' attribute. The literals are from the enumeration {@link org.eclipse.emf.examples.extlibrary.BookCategory}. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Category</em>' attribute isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Category</em>' attribute isn't clear, there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Category</em>' attribute.
 	 * @see org.eclipse.emf.examples.extlibrary.BookCategory
 	 * @see #isSetCategory()
@@ -99,12 +93,12 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	BookCategory getCategory();
-
+	
 	/**
-	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Category</em>' attribute.
+	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Category</em>' attribute.
 	 * @see org.eclipse.emf.examples.extlibrary.BookCategory
 	 * @see #isSetCategory()
 	 * @see #unsetCategory()
@@ -112,22 +106,20 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	void setCategory(BookCategory value);
-
+	
 	/**
-	 * Unsets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Unsets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @see #isSetCategory()
 	 * @see #getCategory()
 	 * @see #setCategory(BookCategory)
 	 * @generated
 	 */
 	void unsetCategory();
-
+	
 	/**
-	 * Returns whether the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute is set.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Returns whether the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getCategory <em>Category</em>}' attribute is set. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return whether the value of the '<em>Category</em>' attribute is set.
 	 * @see #unsetCategory()
 	 * @see #getCategory()
@@ -135,16 +127,14 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	boolean isSetCategory();
-
+	
 	/**
-	 * Returns the value of the '<em><b>Author</b></em>' reference.
-	 * It is bidirectional and its opposite is '{@link org.eclipse.emf.examples.extlibrary.Writer#getBooks <em>Books</em>}'.
-	 * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Author</b></em>' reference. It is bidirectional and its opposite is '{@link org.eclipse.emf.examples.extlibrary.Writer#getBooks <em>Books</em>}'. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Author</em>' reference isn't clear,
-	 * there really should be more of a description here...
+	 * If the meaning of the '<em>Author</em>' reference isn't clear, there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the value of the '<em>Author</em>' reference.
 	 * @see #setAuthor(Writer)
 	 * @see org.eclipse.emf.examples.extlibrary.EXTLibraryPackage#getBook_Author()
@@ -153,15 +143,15 @@ public interface Book extends CirculatingItem {
 	 * @generated
 	 */
 	Writer getAuthor();
-
+	
 	/**
-	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getAuthor <em>Author</em>}' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @param value the new value of the '<em>Author</em>' reference.
+	 * Sets the value of the '{@link org.eclipse.emf.examples.extlibrary.Book#getAuthor <em>Author</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value
+	 *            the new value of the '<em>Author</em>' reference.
 	 * @see #getAuthor()
 	 * @generated
 	 */
 	void setAuthor(Writer value);
-
+	
 } // Book
